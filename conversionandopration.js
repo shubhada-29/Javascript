81d0427a8db78b23855ad243d80adcc0185fcd4d
shubhada-29 <bshubhada2448@gmail.com>
2024-09-14 12:21:18 +0000

    conversionDone

---
+++ conversionandopration.js
@@ -0,0 +1,40 @@
+// let score = 33
+// let score = "33asd"
+// let score = null
+// let score = undefined
+//  let score = true
+   let score = "shubhada"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+//conversion in number
+
+// let valueInNumber = Number(score)  //capital format
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);   //when got input number and character it will conver into number but it is actually NaN
+
+
+/*
+"33" => 33
+"33asd" => NaN
+true => 1; false => 0
+*/
+
+//conversion in boolean
+let isLoggedIn = 0
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "shubhada" => true
+
+//conversion in string
+
+let A = 29
+let stringNumber = String(A)
+console.log(A);
+console.log(typeof stringNumber);
+
