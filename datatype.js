3e984df9374aaeaed1c03a16988b46b1632f2b33
shubhada-29 <bshubhada2448@gmail.com>
2024-09-14 06:18:47 +0000

    variabledone

---
+++ datatype.js
@@ -0,0 +1,24 @@
+// "use strict"; //treat all js code as newer version 
+
+//alert( 3 + 3 ) //we are using nodejs,not browser
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+// null =>standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof "shubhada");
+console.log(typeof age);
+
+console.log(typeof undefined);
+console.log(typeof null);
