e74e8ad2878307368eab133320c80248eeaa9a11
shubhada-29 <bshubhada2448@gmail.com>
2024-09-16 07:24:02 +0000

    stringsDone

---
+++ 06_stringinjs.js
@@ -0,0 +1,26 @@
+const name = "Global"
+const repoCount = 150
+
+//console.log(`Hello my name is ${name} and my recount is ${repoCount}`);
+
+const gameName = new String('Global')
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCaseCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newstr = "  Global  "
+console.log(newstr);
+console.log(newstr.trim());
+
+const strnew = "Global"
+console.log(strnew.replace('l', 'D'))
+console.log(strnew.includes('global'))
