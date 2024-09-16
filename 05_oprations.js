4ed4abe2c77aa553f86befd3b7a9a112a1138add
shubhada-29 <bshubhada2448@gmail.com>
2024-09-16 05:29:45 +0000

    oprationsDone

---
+++ 05_oprations.js
@@ -0,0 +1,52 @@
+let value = 3
+let negValue = -value
+ //console.log(negValue);
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/2);
+//  console.log(2%2);
+
+// let str1 = "welcome"
+// let str2 = " shubhada"
+
+// let str3 = str1 + str2  //concatination oprator
+// console.log(str3);
+
+let num1, num2, num3;
+// console.log(num1 = num2 = num3 = 1 + 2);
+
+//If used postfix, with operator after operand (for example, x++),
+ //the increment operator increments and returns the value before incrementing.
+ let x = 3;
+ const y = x++;
+ // x is 4; y is 3
+//  console.log(x);
+//  console.log(y);
+ 
+ let x2 = 3n;
+ const y2 = x2++;
+ // x2 is 4n; y2 is 3n
+//  console.log(x2);
+//  console.log(y2);
+
+
+//If used prefix, with operator before operand (for example, ++x), 
+//the increment operator increments and returns the value after incrementing.
+let A = 3;
+const B = ++A;
+// A is 4; B is 4
+//console.log(A);
+//console.log(B);
+
+
+let A2 = 3n;
+const c2 = ++A2;
+// A2 is 4n; B2 is 4n
+ console.log(A2);
+ console.log(c2);
+
+ //link to study
+ //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
