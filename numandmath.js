712b7bc0d85f20654d3202e82a5f1397805f6d0a
shubhada-29 <bshubhada2448@gmail.com>
2024-09-18 05:18:42 +0000

    numDone

---
+++ numandmath.js
@@ -0,0 +1,22 @@
+const score = 400
+//console .log(score);
+
+const balance = new Number(100)
+//console.log(balance)
+
+console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const num = 123.8988
+//console.log(num.toPrecision(4));
+
+const num2 = 1000000
+//console.log(num2.toLocaleString())
+//console.log(num2.toLocaleString('en-In')); //indian courency
+
+//++++++++++++++ MATH +++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
